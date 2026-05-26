58a695cf4ce02727163cf3c3ba796a81fc92e14e
Rohith <rohithchitti2226@gmail.com>
2026-05-26 19:34:32 +0530

    fixed allowed hosts for render

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s$%-b=03ocw8$sj2m$)j7(oc-!95qm6afgeyyivv$k@s-*a$-l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "breathe-esg-platform-hmrs.onrender.com",
+    "127.0.0.1",
+    "localhost"
+]
 
 
 # Application definition
