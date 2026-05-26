5547cd959c6a9356335504c3e71ae49b86d3fb3f
Rohith <rohithchitti2226@gmail.com>
2026-05-26 19:22:44 +0530

    fixed upload csv indentation

--- backend/config/emissions/upload_views.py
+++ backend/config/emissions/upload_views.py
@@ -1,7 +1,5 @@
-
 import pandas as pd
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.decorators import parser_classes
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -12,20 +10,21 @@ from .models import (
 )
 
 
-@api_view(['GET', 'POST'])
-@parser_classes([MultiPartParser, FormParser])
+@api_view(['POST'])
 def upload_csv(request):
 
     file = request.FILES.get('file')
 
     if not file:
         return Response({
-            'error': 'No file uploaded'
+            "error": "No file uploaded"
         }, status=400)
 
     df = pd.read_csv(file)
 
-    organization = Organization.objects.first()
+    organization, _ = Organization.objects.get_or_create(
+        name="Breathe ESG Demo Org"
+    )
 
     data_source = DataSource.objects.create(
         organization=organization,
@@ -33,48 +32,25 @@ def upload_csv(request):
         file_name=file.name
     )
 
-    created_records = []
+    for _, row in df.iterrows():
 
-   for _, row in df.iterrows():
+        quantity = float(row['quantity'])
 
-    quantity = float(row['quantity'])
+        suspicious = quantity > 10000
 
-    unit = row['unit']
-
-    normalized_quantity = quantity
-    normalized_unit = unit
-
-    # Unit normalization
-
-    if unit.lower() == "gallons":
-
-        normalized_quantity = quantity * 3.78541
-        normalized_unit = "Liters"
-
-    elif unit.lower() == "kwh":
-
-        normalized_quantity = quantity / 1000
-        normalized_unit = "MWh"
-
-    suspicious = normalized_quantity > 10000
-
-    record = EmissionRecord.objects.create(
-        organization=organization,
-        data_source=data_source,
-        scope='SCOPE_1',
-        activity_type=row['activity_type'],
-        quantity=quantity,
-        unit=unit,
-        normalized_quantity=normalized_quantity,
-        normalized_unit=normalized_unit,
-        record_date=row['record_date'],
-        is_suspicious=suspicious,
-        status='PENDING'
-    )
-
-    created_records.append(record.id)
+        EmissionRecord.objects.create(
+            organization=organization,
+            data_source=data_source,
+            scope=row['scope'],
+            activity_type=row['activity_type'],
+            quantity=quantity,
+            unit=row['unit'],
+            normalized_quantity=quantity,
+            normalized_unit=row['unit'],
+            record_date=row['record_date'],
+            is_suspicious=suspicious
+        )
 
     return Response({
-        'message': 'CSV uploaded successfully',
-        'records_created': created_records
+        "message": "CSV uploaded successfully"
     })
